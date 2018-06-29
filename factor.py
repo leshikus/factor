b14ebda3645909b8f7d82097957066a84a5a2489
Alexei Fedotov <alexei.fedotov@gmail.com>
2018-06-30 02:47:55 +0300

    final solution (ugly)

--- factor.py
+++ factor.py
@@ -27,70 +27,100 @@ def brent(n):
 
     return g
 
-def factor(n, pd):
+def factor(n):
     global timeout
+    if timeout: return [n]
 
-    for i in range(5):
-        d = brent(n)
-        if d > 1 and d < n:
-            factor(n // d, pd)
-            factor(d, pd)
-            return
+    d = brent(n)
+    if d == 1 or d == n: return [n]
+    else: return factor(n // d) + factor(d)
 
-        if timeout: break
+n = int(input())
 
-    if n in pd: pd[n] += 1
-    else: pd[n] = 1
+primes = [ 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587, 593, 599, 601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691, 701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797, 809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883, 887, 907, 911, 919, 929, 937, 941,  947, 953, 967, 971, 977, 983, 991, 997 ]
 
-def factor_input():
-    global alpha, n_uniq_primes
-    n = int(input())
+alpha = []
 
-    primes = [ 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61,
-	67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139,
-	149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227,
-	229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311,
-	313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401,
-	409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491,
-	499, 503, 509, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587, 593, 599,
-	601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683,
-	691, 701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797,
-	809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883, 887,
-	907, 911, 919, 929, 937, 941,  947, 953, 967, 971, 977, 983, 991, 997 ]
+n_uniq_primes = 0
 
-    alpha = []
-    n_uniq_primes = 0
+def cancel_opt():
+    global timeout
+    timeout = True
 
-    for d in primes:
-        if n % d == 0:
+timeout = False
+wd = threading.Timer(1.5, cancel_opt, ())
+wd.start()
+
+for d in primes:
+    if n % d == 0:
+        n //= d
+
+        if n %d != 0:
+            n_uniq_primes += 1
+            continue
+
+        n //= d
+        k = 2
+        while n % d == 0:
             n //= d
+            k += 1
+        alpha.append(k)
 
-            if n % d != 0:
-                n_uniq_primes += 1
-                if n == 1: break
-                continue
-
-            n //= d
-            k = 2
-            while n % d == 0:
-                n //= d
-                k += 1
-            alpha.append(k)
-            if n == 1: break
-
-    if n > 1:
-        pd = {}
-        factor(n, pd)
-        for k in pd.values():
+if n > 1:
+    p2 = factor(n)
+    p2.sort()
+    p2.append(0)
+    k = 1
+    for d in range(0, len(p2) - 1):
+        if p2[d] == p2[d + 1]: k += 1
+        else:
             if k == 1: n_uniq_primes += 1
             else: alpha.append(k)
+            k = 1
 
-    alpha.sort(reverse = True)
-    return tuple(alpha)
+l = len(alpha)
+salpha = sum(alpha)
+alpha.sort(reverse = True)
 
 def e_opt(n):
     d = math.sqrt(1 + 8 * n) - 1
     return math.floor(d * 0.5)
+
+best = sum(e_opt(alpha[i]) for i in range(l))
+
+if l <= 1:
+    print(n_uniq_primes + best)
+    wd.cancel()
+    exit(0)
+
+f = []
+for i in range(l):
+    s = [0] * l
+    s[i] = 1
+    f.append(tuple(s))
+
+list_factors = [0, f]
+list_factors_set = [0, set(f)]
+sys.setrecursionlimit(20000)
+
+def a_minus(a, b):
+    global l
+
+    r = [0] * l
+    for i in range(l):
+        c = a[i] - b[i]
+        if c < 0: return None
+        r[i] = c
+    return tuple(r)
+
+debug_stack = []
+debug_stack = None # comment this to debug
+best_stack = []
+
+#print(alpha, salpha)
+
+bestd = {}
+dict_opts = 0
 
 def get_list_factors_set(ssum):
     global alpha, list_factors, list_factors_set
@@ -117,18 +147,8 @@ def get_list_factors_set(ssum):
 
     return fs
 
-def a_minus(a, b):
-    global l
-
-    r = [0] * l
-    for i in range(l):
-        c = a[i] - b[i]
-        if c < 0: return None
-        r[i] = c
-    return tuple(r)
-
 def optimize(cur, scur, sindex, ssum, nf):
-    global alpha, best, l, list_factors
+    global alpha, best, l, list_factors, rec_depth
     global debug_stack, best_stack, dict_opts, timeout
 
     if timeout: return -1
@@ -200,45 +220,7 @@ def optimize(cur, scur, sindex, ssum, nf):
     bestd[cur, sindex, ssum] = bestc
     return bestc
 
-def cancel_run():
-    global timeout
-    timeout = True
-
-timeout = False
-wd = threading.Timer(1.7, cancel_run, ())
-wd.start()
-
-alpha = factor_input()
-l = len(alpha)
-salpha = sum(alpha)
-
-best = sum(e_opt(alpha[i]) for i in range(l))
-
-if l <= 1:
-    print(n_uniq_primes + best)
-    wd.cancel()
-    exit(0)
-
-f = []
-for i in range(l):
-    s = [0] * l
-    s[i] = 1
-    f.append(tuple(s))
-
-list_factors = [0, f]
-list_factors_set = [0, set(f)]
-sys.setrecursionlimit(20000)
-
-debug_stack = []
-debug_stack = None # comment this to debug
-best_stack = []
-
-#print(alpha, salpha)
-
-bestd = {}
-dict_opts = 0
-
-r = optimize(alpha, salpha, 0, 1, 0)
+r = optimize(tuple(alpha), salpha, 0, 1, 0)
 if not debug_stack is None:
     print('best', best, best_stack)
     print('dict', len(bestd), dict_opts)
